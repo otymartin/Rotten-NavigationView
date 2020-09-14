a7746a47ea9e60727b8173802ede9955d4658c53
Martin Otyeka <otyeka.martin@gmail.com>
2020-09-14 13:51:45 -0400

    Description

--- Rotten NavigationView/TabViews.swift
+++ Rotten NavigationView/TabViews.swift
@@ -12,7 +12,9 @@ struct FirstView: View {
     
     var body: some View {
         NavigationView {
-            Text("First Page")
+            Text("Notice the NavigationBar is not laid out properly. Tap the Second Tab then come back to the First. It will be fixed, Why?🤔")
+                .padding([.leading, .trailing], 32)
+                .multilineTextAlignment(.center)
                 .navigationBarTitle("", displayMode: .inline)
                 .navigationBarItems(leading: Text("First").font(Font.system(size: 24, weight: .bold)))
         }
@@ -23,7 +25,9 @@ struct SecondView: View {
     
     var body: some View {
         NavigationView {
-            Text("Second Page")
+            Text("Notice that the NavigationBar on this view is properly laid out. Go back to the First View and it will be fixed.")
+                .padding([.leading, .trailing], 32)
+                .multilineTextAlignment(.center)
                 .navigationBarTitle("", displayMode: .inline)
                 .navigationBarItems(leading: Text("Second").font(Font.system(size: 24, weight: .bold)))
         }
